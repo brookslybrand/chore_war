2dd6b4d1004fb5293af62dbbb47ee2a6d2f536df
Brooks <brooks.lybrand@business-laboratory.com>
2018-07-30 18:19:35 -0500

    Add Login Activity

---
+++ Final_Project/app/src/main/java/brookslybrand/chorewar/LoginActivity.java
@@ -0,0 +1,73 @@
+package brookslybrand.chorewar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.database.Cursor;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    // declare_auth
+//    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "Starting Login Activity");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // email login button event
+        findViewById(R.id.email_login_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailPasswordActivity = new Intent(LoginActivity.this, EmailPasswordActivity.class);
+                startActivity(emailPasswordActivity);
+            }
+        });
+
+        findViewById(R.id.google_login_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailPasswordActivity = new Intent(LoginActivity.this, EmailPasswordActivity.class);
+                startActivity(emailPasswordActivity);
+            }
+        });
+
+        findViewById(R.id.facebook_login_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailPasswordActivity = new Intent(LoginActivity.this, EmailPasswordActivity.class);
+                startActivity(emailPasswordActivity);
+            }
+        });
+
+//        mAuth = FirebaseAuth.getInstance();
+    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+////        updateUI(currentUser);
+//    }
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Chore War
+CS 329E Elements of Mobile Computing Final Project (Android App)
+
+Chore War is an app to help roommates and families schedule and assign chores in a fair, frequent, and fun way. After transition from having 4 brothers, to 1 college roommate in a dorm, to 4 roommates in an apartment, to 5 roommates in a house, I’ve learned that humans have the amazing ability to be messy. Humans also love to compete. If you combine those two together, you get Chore War.
+
+
+Chore War is an app that helps you organize and keep track of the chores that need to be done by you and the people you live with to keep your living space clean. But it isn’t just a productivity app. Instead, Chore War turns cleaning and cohabitating into competitive fun. By setting rewards and consequences for the best and worst Chore Warriors, doing your regular ol’ household maintenances can now be exciting and the source of a lot of fun.
+
+
+Chore War is not only for messy college roommates; families, couples, and even entire offices can also get in on the fun. If you’ve got surfaces that need cleaning, spaces that needed decluttering, and egos that need stroking, then you need Chore War.
